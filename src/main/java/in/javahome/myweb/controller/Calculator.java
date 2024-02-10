135e497132f4584d929bc8e0fef6cf36dd332f8c
AsmaAlam <158472271+AsmaAlam@users.noreply.github.com>
2024-02-10 08:31:07 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -7,17 +7,16 @@ public class Calculator {
 	/*
 	 * @param i
 	 * @param j
-  	 * @param k
 	 * @return int
 	 */
-	public int add(int i, int j, int k){
-		return i+j+k;
+	public int add(int i, int j){
+		return i+j;
 	}
-	public int multiply(int i, int j, int k){
-		return i*j*k;
+	public int multiply(int i, int j){
+		return i*j;
 	}
 	
-	public int sub(int i, int j, int k){
-		return i-j-k;
+	public int sub(int i, int j){
+		return i-j;
 	}
 }
